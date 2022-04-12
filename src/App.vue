1a175ea3325a7cf6fd3749cd25ac577020d90c49
roma_marshall <roma_marshall@outlook.com>
2022-04-12 14:59:30 +0200

    login page edit

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,17 @@ import Logo from './assets/img/logo.svg'
 <template>
   <div class="flex justify-between min-h-screen font-sans">
     <div class="w-1/2 bg-cover">
-      <div class="w-1/2">
+      <div class="w-2/2">
         <div class="flex flex-col px-24 pt-10">
           <Logo />
+          <div class="pt-20 pb-6">
+            <h1 class="text-3xl font-bold tracking-wide leading-loose">
+              hello @username
+            </h1>
+            <span>
+              Login below via username & password
+            </span>
+          </div>
         </div>
       </div>
     </div>
