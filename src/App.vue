728f14858bbf7ca88ede1c50832320792819e5b5
roma_marshall <roma_marshall@outlook.com>
2022-04-12 14:48:45 +0200

    svg logo aded

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <script setup>
+import Logo from './assets/img/logo.svg'
 
 </script>
 
 <template>
-  <h1 class="text-red-300">hello world</h1>
+  <div class="flex justify-between min-h-screen font-sans">
+    <div class="w-1/2 bg-cover">
+      <div class="w-1/2">
+        <div class="flex flex-col px-24 pt-10">
+          <Logo class="w-32" />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
