c517db798d774968834ce7460bbb4fbbf39ff568
roma_marshall <roma_marshall@outlook.com>
2022-04-14 14:45:52 +0200

    svg added & input area

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <script setup>
 import Logo from './assets/img/logo.svg'
+import Mail from './assets/img/mail.svg'
+import Password from './assets/img/password.svg'
 
 </script>
 
@@ -8,7 +10,7 @@ import Logo from './assets/img/logo.svg'
     <div class="w-1/2 bg-cover">
       <div class="w-2/2">
         <div class="flex flex-col px-24 pt-10">
-          <Logo />
+          <Logo/>
           <div class="pt-20 pb-6">
             <h1 class="text-3xl font-bold tracking-wide leading-loose">
               hello @username
@@ -18,10 +20,42 @@ import Logo from './assets/img/logo.svg'
             </span>
           </div>
           <div class="flex justify-between items-center pt-6">
-            <hr class="w-full border-gray-400" />
+            <hr class="w-full border-gray-400"/>
             <span class="px-4 font-light tracking-wider text-gray-500">*</span>
-            <hr class="w-full border-gray-400" />
+            <hr class="w-full border-gray-400"/>
           </div>
+          <form action="">
+            <div class="pt-6">
+              <label for="email" class="font-light">email</label>
+              <div class="flex items-center mt-2 w-full rounded-lg border-gray-400">
+                <div>
+                  <Mail class="w-6 h-6 pointer-events-none"/>
+                </div>
+                <input
+                    type="text"
+                    name="email"
+                    id="email"
+                    value="no@mail.su"
+                    class="px-4 py-4.5 w-full focus:outline-none font-light border-0 focus:ring-0"
+                />
+              </div>
+            </div>
+            <div class="pt-6">
+              <label for="email" class="font-light">password</label>
+              <div class="flex items-center mt-2 w-full rounded-lg border-gray-400">
+                <div>
+                  <Password class="w-6 h-6 pointer-events-none"/>
+                </div>
+                <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    value="no@mail.su"
+                    class="px-4 py-4.5 w-full focus:outline-none font-light border-0 focus:ring-0"
+                />
+              </div>
+            </div>
+          </form>
         </div>
       </div>
     </div>
